c5b4047c15f885a510ea03250cd6da44485ebe13
TienPJO <ductien.hust@gmail.com>
2020-02-25 10:59:54 +0700

    first commit

---
+++ precache-manifest.9815e9109fc8ac6ddf2455396a7cf2db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a690b05240ebcf19a96eed50b303560e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7522dcddf2c6068ddd52",
+    "url": "/static/css/main.ef517511.chunk.css"
+  },
+  {
+    "revision": "d6fc35459b43d85d6ca5",
+    "url": "/static/js/2.5cbb1973.chunk.js"
+  },
+  {
+    "revision": "7522dcddf2c6068ddd52",
+    "url": "/static/js/main.ad653e6c.chunk.js"
+  },
+  {
+    "revision": "9e3020ed628ad0937616",
+    "url": "/static/js/runtime-main.9944f575.js"
+  }
+]);
